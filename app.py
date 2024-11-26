ed81e36fb64adb51fc152782233719574f2a3a6d
ss <os>
2024-11-26 18:22:15 +0100

    file version

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 import redis
-import json
-from movingHead import MovingHead
+import random
+import time
+from websocket import create_connection, WebSocketException
 from utils import json_dmx_parser
+
+ws = create_connection("ws://127.0.0.1:9999/qlcplusWS")
+
+def send_messages(package):
+        try:
+            for message in package:
+                if message['channel'] == 19 or  message['channel'] == 20 or  message['channel'] == 39 or  message['channel'] == 40:
+                    continue  
+                formated_message = f"CH|{message['channel']}|{message['value']}"
+                ws.send(formated_message)
+        except WebSocketException as e:
+            print(f"WebSocket Handshake: {e}")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
 
 def connect_to_redis():
     try:
@@ -19,7 +34,19 @@ def connect_to_redis():
         return None
 
 redis_client = connect_to_redis()
-movinghead = MovingHead()
+print(redis_client.get("3:fixyou:so"))
 
-# read from redis
-# move the Moving head
+while True:    
+    try:
+        # data = redis_client.get("3:fixyou:so")
+        # if data and data["Status"] != "Stop":
+            # get the package using the transformation function
+            # Transfom the AI Json data to Pin instructions 
+            package = [{"channel": number, "value": int (random.random() * 255)} for number in range(1, 41)]
+            send_messages(package=package)
+            time.sleep(0.5)
+
+    except KeyboardInterrupt:
+            package = [{"channel": number, "value": 0} for number in range(1, 41)]
+            send_messages(package=package)
+            break
